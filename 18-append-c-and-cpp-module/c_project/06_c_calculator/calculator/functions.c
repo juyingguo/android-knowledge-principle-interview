a377f0f77dd0fabde704dd194c8fb06ae7e65d55
juyingguo <juying2050@sina.cn>
2020-04-05 19:51:00 +0800

    add calculate project [should delet .git dir in project]

---
+++ 18-append-c-and-cpp-module/c_project/06_c_calculator/calculator/functions.c
@@ -0,0 +1,15 @@
+#include "functions.h"
+
+//a function implementation
+int add(int a, int b){
+	return a+b;
+}
+int subs(int a, int b){
+	return a-b;
+}
+int prod(int a, int b){
+	return a*b;
+}
+int divid(int a, int b){
+	return a/b;
+}
