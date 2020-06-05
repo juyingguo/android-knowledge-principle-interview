4a229ec130053e80fcbd9932c897fc34eb225b56
juyingguo <juying2050@sina.cn>
2020-06-05 16:42:09 +0800

    update .gitignore; not ignore .gitignore

---
+++ 18-append-c-and-cpp-module/cpp_project/04_c++_programming_2th/teaching_material/c4-4-02.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+ int max(int,int, int);
+
+int main()
+{
+     int i=10,j=20,k=30,m;
+
+     m=max(i,j,k);
+
+     cout<<"max="<<m<<endl;
+
+     return 0;
+}
+ inline int max(int a,int b,int c)
+{
+    if(b>a) a=b;
+
+    if(c>a) a=c;
+
+    return a;
+}
+
+
