8e0e0c33ea994abb68d40f5a9b30af76ae7ca345
juyingguo <juying2050@sina.cn>
2020-09-04 16:01:38 +0800

    add radasm tool

--- 18-append-c-and-cpp-module/c_project/05_c_base_knowledge/016_thread_for_windows_pass_argument_test.c
+++ 18-append-c-and-cpp-module/c_project/05_c_base_knowledge/016_thread_for_windows_pass_argument_test.c
@@ -3,7 +3,7 @@
 版权声明：本文为CSDN博主「简渲」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
 原文链接：https://blog.csdn.net/tianzry/article/details/61933362
 
-*/  
+*/
 
 #include <stdio.h>
 #include <windows.h>
@@ -19,19 +19,19 @@ void fun2(void *pArg){
         if(a>=10) break;
         Sleep(200);
         printf("a = %d, thead running...\n", a);
-    } 
+    }
     printf("new thread end.\n\n");
     _endthread();
 }
 
 int main(){
-    int b = 5;
+    int b = 6;
     int* arg = &b;
     _beginthread(fun2, 0, (void *)arg);
     while(1){
         if(a >= 10){
             printf("main thread execute finish\n");
-            break; 
+            break;
         }
         //Sleep(300);
 	}
