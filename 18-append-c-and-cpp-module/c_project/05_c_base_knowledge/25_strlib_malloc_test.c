d5b69cfd120979382a0845f15864c234b46c55d6
juyingguo <juying2050@sina.cn>
2020-10-04 16:41:37 +0800

    add malloc

---
+++ 18-append-c-and-cpp-module/c_project/05_c_base_knowledge/25_strlib_malloc_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+   char *str;
+
+   /* 最初的内存分配 */
+   str = (char *) malloc(1);
+   strcpy(str, "runoob");
+   printf("String = %s,  Address = %u\n", str, str);
+   printf("String = %s,  Address = %d\n", str, str);
+
+   /* 重新分配内存 */
+   str = (char *) realloc(str, 1);
+   strcat(str, ".com");
+   printf("String = %s,  Address = %u\n", str, str);
+
+   free(str);
+
+   return(0);
+}
