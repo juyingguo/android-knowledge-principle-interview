130897c80aacec62b11cb4b5f64dccce9679a2dd
juyingguo <juying2050@sina.cn>
2021-01-13 17:48:29 +0800

    append js overload function

--- 18-append-c-and-cpp-module/cpp_project/02_cpp_base_knowledge/033_cast_01_static_cast_01.cpp
+++ 18-append-c-and-cpp-module/cpp_project/02_cpp_base_knowledge/033_cast_01_static_cast_01.cpp
@@ -6,11 +6,13 @@ static_cast 类型转换
 using namespace std;
 
 int main() {
-    int* pi = NULL;
+    int a = 100;
+    int* pi = &a;
     //char c = int(pi); //C++类型转换
     //char c = (int)pi; //C类型转换
     void* pc = pi;
     pi = static_cast<int *>(pc);
-    char c = static_cast<int>(pi); //会出现报错，因为int* 无法转换为int类型
+    cout<<"pi = "<<pi<<"\n*pi="<<*pi<<endl;
+    int *c = static_cast<int*>(pi); //会出现报错，因为int* 无法转换为int类型
 
 }
