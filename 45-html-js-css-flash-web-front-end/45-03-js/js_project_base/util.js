434f4342d5673adb9063ccfc30d701d583eb2340
juyingguo <juying2050@sina.cn>
2021-03-19 15:57:38 +0800

    refactor cpp record file

--- 45-html-js-css-flash-web-front-end/45-03-js/js_project_base/util.js
+++ 45-html-js-css-flash-web-front-end/45-03-js/js_project_base/util.js
@@ -3,3 +3,12 @@
 				alert("hello1");
 			}
 		// </script>
+		
+String.prototype.compare = function(str) {
+	//不区分大小写
+	if(this.toLowerCase() == str.toLowerCase()) {
+		return true;
+	} else {
+		return false;
+	}
+}
