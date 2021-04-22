e5c4c6bd39bd60d155c805ddda6901237163dbba
juyingguo <juying2050@sina.cn>
2021-04-22 14:30:51 +0800

    cpp: myudp cross plateform adapter.

---
+++ 18-c-and-cpp/cpp/itcast_cpp5term/02_cppjiuye/20140808/myudp_cross_platform/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "udp.h"
+
+using namespace std;
+
+int main(int argc,char *args[])
+{
+    if(argc >1){
+        socket_send(args[1]);
+    }else {
+        socket_recv();
+    }
+    cout << "exit!" << endl;
+    return 0;
+}
